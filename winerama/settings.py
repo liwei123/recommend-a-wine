59334b47efd996ef35dc885523162caa390a728a
liwei123 <liwei13193@hotmail.com>
2018-08-20 16:46:39 +0800

    config for deployment on heroku

--- winerama/settings.py
+++ winerama/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -132,3 +133,5 @@ LOGIN_REDIRECT_URL = '/reviews/review/user'
 
 ACCOUNT_ACTIVATION_DAYS = 7 # One-week activation window
 REGISTRATION_AUTO_LOGIN = True # Automatically log the user in.
+
+django_heroku.settings(locals())
